809de03b5cf1415bc501bd702abe3a6f292c26f6
cok agung yudi <cokagungyudi@gmail.com>
2026-01-07 02:44:41 +0900

    Add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,8 +4,9 @@ import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
 import { ThemeProvider } from 'next-themes';
 import {useState} from 'react';
-import { GoogleTagManager } from '@next/third-parties/google'
-import Head from 'next/head'
+import { GoogleTagManager } from '@next/third-parties/google';
+import { Analytics } from "@vercel/analytics/next"
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [buttonText, setButtonText] = useState('Hello !');
@@ -24,6 +25,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Navigation onChangeLanguage={onChangeLanguageHandler} text={buttonText} />
       <Component {...pageProps} text={buttonText}/>
       <GoogleTagManager gtmId={'GTM-WVZ645LG'} />
+      <Analytics />
       <Footer/>
       </ThemeProvider>
     </>
